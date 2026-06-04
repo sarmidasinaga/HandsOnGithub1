8440a0b7155518360df3b9db7e7f7d02700ddaac
Yusuf Dwi Kurniawan <yusufdwikurniawan01@gmail.com>
2026-06-04 10:50:19 +0700

    Add Hallo Mida

--- HandsOnGithub1/ContentView.swift
+++ HandsOnGithub1/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("This is a new feature")
             Text("This is profile")
+            Text("Halo Mida")
         }
         .padding()
     }
