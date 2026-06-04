1afc86dc68df13316f63b4d2d6062384603bff8d
Mida <sarmidaulisinaga@gmail.com>
2026-06-04 09:29:55 +0700

    Initial Commit

---
+++ HandsOnGithub1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HandsOnGithub1
+//
+//  Created by Sarmida Uli Sinaga on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HandsOnGithub1/HandsOnGithub1App.swift
@@ -0,0 +1,17 @@
+//
+//  HandsOnGithub1App.swift
+//  HandsOnGithub1
+//
+//  Created by Sarmida Uli Sinaga on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HandsOnGithub1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
