b09c88b40445fe34ac882cc24187d3ae79cd717d
Jonathan Limanza <jonathanlimanza@gmail.com>
2026-06-04 10:47:01 +0700

    Add feature B

---
+++ HandsOnGithub1/B-feature.swift
@@ -0,0 +1,18 @@
+//
+//  B-feature.swift
+//  HandsOnGithub1
+//
+//  Created by Jon on 04/06/26.
+//
+
+import SwiftUI
+
+struct B_feature: View {
+    var body: some View {
+        Text("This is B feature")
+    }
+}
+
+#Preview {
+    B_feature()
+}
