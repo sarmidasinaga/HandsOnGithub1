f0aa2f56778e53fd1dce105d52ceb7fc81e2789a
Jonathan Limanza <jonathanlimanza@gmail.com>
2026-06-04 11:09:13 +0700

    Add new Update

--- HandsOnGithub1/ContentView.swift
+++ HandsOnGithub1/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("This is a new feature")
-            Text("This is profile")
+            Text("New uPdate")
         }
         .padding()
     }
